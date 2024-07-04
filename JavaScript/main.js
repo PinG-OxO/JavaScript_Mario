05fb59afcc184949fe7987bd6d27bfddc11031f4
OxO <xxsharutoxx@gmail.com>
2024-07-04 15:13:57 +0900

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,132 @@
+const SCREEN_SIZE_W = 256;      // スクリーンの幅
+const SCREEN_SIZE_H = 224;      // スクリーンの高さ
+const GAME_FPS = 1000/60;       // FPS
+
+// 仮想キャンバスの作成
+let vcan = document.createElement("canvas");
+let vcon = vcan.getContext("2d");
+
+// 実キャンバスの作成
+let can = document.getElementById("can");
+let con = can.getContext("2d");
+
+// フレームレート関連
+let frameCount = 0;
+let startTime;
+
+// 仮想キャンバスのサイズ設定
+vcan.width = SCREEN_SIZE_W;
+vcan.height = SCREEN_SIZE_H;   
+
+// 実キャンバスのサイズ設定
+can.width = SCREEN_SIZE_W * 3;
+can.height = SCREEN_SIZE_H * 3;
+
+con.imageSmoothingEnabled = false;     // モザイクを防ぐ
+
+let chImg = new Image(); chImg.src = "Image/sprite.png";    // 画像を取得
+
+let keyb = {}; // キーボードの状態を保存する連想配列
+
+let ojisan = new Ojisan(100, 150); // おじさんのインスタンスを生成
+
+// 更新処理
+function update()
+{
+    // おじさんの更新
+    ojisan.update();
+}
+
+function drawSprite(snum, x, y)
+{
+    let sx = (snum & 15) << 4;
+    let sy = (snum >> 4) << 4;
+    vcon.drawImage(chImg, sx, sy, 16, 32, x, y, 16, 32);
+}
+
+// 描画処理
+function draw()
+{
+    // 仮想画面のクリア
+    vcon.fillStyle = "#66AAFF";
+    vcon.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H); 
+
+    // おじさんを描画
+    ojisan.draw();
+
+    // デバッグ情報を表示
+    vcon.font = "24px 'Impact' ";
+    vcon.fillStyle = "white";
+    vcon.fillText("Frame: " + frameCount, 10, 20);
+
+    // 仮想画面から実画面へ拡大転送
+    con.drawImage(vcan, 0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, 0, 0, SCREEN_SIZE_W * 3, SCREEN_SIZE_H * 3);
+}
+
+// ループ開始
+window.onload = function()
+{
+    startTime = performance.now();
+    mainLoop();
+}
+
+// メインループ
+function mainLoop()
+{
+    let nowTime = performance.now();
+    let nowFrame = (nowTime - startTime) / GAME_FPS;
+
+    if(nowFrame > frameCount)
+    {
+        let c = 0;
+        while(nowFrame > frameCount)
+        {
+            frameCount++;
+            // 更新処理
+            update();
+            if(++c >= 4)break;
+        }
+
+            // 描画処理
+            draw();
+    }
+    requestAnimationFrame(mainLoop);
+}
+
+// キーボードが押された時
+document.onkeydown = function(e)
+{
+    switch(e.key) {
+        case 'ArrowLeft':
+            keyb.Left = true;
+            break;
+        case 'ArrowRight':
+            keyb.Right = true;
+            break;
+        case 'z':
+            keyb.Bbutton = true;
+            break;
+        case 'x':
+            keyb.Abutton = true;
+            break;
+    }
+}
+
+// キーボードが離された時
+document.onkeyup = function(e)
+{
+    switch(e.key) {
+        case 'ArrowLeft':
+            keyb.Left = false;
+            break;
+        case 'ArrowRight':
+            keyb.Right = false;
+            break;
+        case 'z':
+            keyb.Bbutton = false;
+            break;
+        case 'x':
+            keyb.Abutton = false;
+            break;
+    }
+}
---
+++ JavaScript/ojisan.js
@@ -0,0 +1,129 @@
+// おじさんクラス
+
+const ANIME_STAND = 0;          // 立ちアニメーション
+const ANIME_WALK  = 1;          // 歩きアニメーション
+const ANIME_BRAKE = 2;          // ブレーキアニメーション
+const ANIME_JUMP  = 4;          // ジャンプのアニメーション
+const GRAVITY     = 4;          // 重力
+const MAX_SPEED   = 32;         // 最高速度
+const WALK_LEFT   = 1;          // 左向き
+const WALK_RIGHT  = 0;          // 右向き
+
+class Ojisan {
+    constructor(x, y){
+        this.x  = x<<4;
+        this.y  = y<<4;
+        this.vx = 0;
+        this.vy = 0;
+        this.animeNum  = 0;
+        this.spriteNum = 0;
+        this.animeCount = 0;
+        this.direction = 0;
+        this.jump = 0;
+    }
+
+    // フレームごとの更新処理
+    update(){
+        // アニメーションのカウント
+        this.animeCount++;
+        if(Math.abs(this.vx) === MAX_SPEED) this.animeCount++;
+
+        this.updateAnime();
+        this.updateWalk();
+        this.updateJump();
+
+        // 重力を追加
+        if(this.vy < 64) this.vy += GRAVITY;
+
+        // おじさんの座標を更新
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // 床に着地させる
+        if(this.y > 150<<4) {
+            if(this.animeNum === ANIME_JUMP)this.animeNum = ANIME_WALK;
+            this.jump = 0;
+            this.y = 150<<4;
+            this.vy = 0;
+        }
+    }
+
+    // 移動処理
+
+    updateWalkSub(dir){
+        // 最高速まで加速
+        if(dir === 0 && this.vx <  MAX_SPEED)this.vx++;
+        if(dir === 1 && this.vx > -MAX_SPEED)this.vx--;
+
+        if(!this.jump){
+            // 歩きアニメ
+            this.animeNum   = ANIME_WALK;
+            // 方向転換
+            this.direction  = dir;
+            // 逆キー入力でブレーキ
+            if(this.vx < 0 && dir === WALK_RIGHT)this.vx++;
+            if(this.vx > 0 && dir ===  WALK_LEFT)this.vx--;
+            // 強いブレーキのときブレーキアニメ
+            if(this.vx > 8 && dir ===  WALK_LEFT ||
+               this.vx < -8 && dir === WALK_RIGHT)
+                this.animeNum = ANIME_BRAKE;
+            // 立ちアニメのときカウンタリセット
+            if(this.animeNum === 0)this.animeCount = 0;
+        }
+    }
+
+    updateWalk(){
+        // 左右の移動
+        if(keyb.Left) {
+            this.updateWalkSub(WALK_LEFT);
+        }
+        else if(keyb.Right) {
+            this.updateWalkSub(WALK_RIGHT);
+        }
+        else {
+            if(!this.jump){
+                if(this.vx > 0)this.vx--;
+                if(this.vx < 0)this.vx++;
+                if(!this.vx)this.animeNum = ANIME_STAND;
+            }
+        }
+    }
+
+    // ジャンプ処理
+    updateJump(){
+        if(keyb.Abutton) {
+            if(this.jump === 0) {
+                this.animeNum = ANIME_JUMP;
+                this.jump = 1;
+            }
+            if(this.jump < 15)this.vy = -(64 - this.jump);
+        }
+        if(this.jump)this.jump++;
+    }
+
+    // アニメーション処理
+    updateAnime(){
+        // スプライトの決定
+        switch(this.animeNum) {
+            case ANIME_STAND:
+                this.spriteNum = 0;
+                break;
+            case ANIME_WALK:
+                this.spriteNum = 2 + (this.animeCount/6) % 3;
+                break;
+            case ANIME_BRAKE:
+                this.spriteNum = 5;
+                break;
+            case ANIME_JUMP:
+                this.spriteNum = 6;
+                break;
+        }
+        // 向きの決定
+        if(this.direction)this.spriteNum += 48;
+    }
+
+    // フレームごとの描画処理
+    draw(){
+        drawSprite(this.spriteNum, this.x>>4, this.y>>4);
+    }
+}
